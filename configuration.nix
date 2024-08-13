7fbe303e0a46578da89ca1b9961ec2859a0225b3
Solito Reyes III <solrey3@solrey3.com>
2024-08-13 04:29:20 -0400

    update system packages

--- configuration.nix
+++ configuration.nix
@@ -107,15 +107,17 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   neovim
    wget
    git
    curl
    lsof
    pavucontrol
+   just
   ];
 
   # Set the default editor to vim
-  environment.variables.EDITOR = "vim";
+  environment.variables.EDITOR = "nvim";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- home.nix
+++ home.nix
@@ -40,22 +40,37 @@
     gcc
     util-linux
 
-    neofetch
-    nnn # terminal file manager
-
-    # archives
-    zip
-    xz
-    unzip
-    p7zip
-
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
+    # my shieeeeet
+    cowsay
+    fzf # A command-line fuzzy finder
+    htop
     jq # A lightweight and flexible command-line JSON processor
+    kubectl
+    lazygit
+    # vimPlugins.LazyVim
+    lsof # list open files
+    neofetch
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })  
+    nmap # A utility for network discovery and security auditing
+    nodejs
+    ripgrep # recursively searches directories for a regex pattern
+    stow
+    tmux
+    tokei
+    tree
+    unzip
+    which
+    yarn
+    zip
+    
+    # Extra stuff recommended
+    nnn # terminal file manager
+    # archives
+    xz
+    p7zip
+    # utils
     yq-go # yaml processor https://github.com/mikefarah/yq
     eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
-
     # networking tools
     mtr # A network diagnostic tool
     iperf3
@@ -63,39 +78,29 @@
     ldns # replacement of `dig`, it provide the command `drill`
     aria2 # A lightweight multi-protocol & multi-source command-line download utility
     socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
     ipcalc  # it is a calculator for the IPv4/v6 addresses
-
     # misc
-    cowsay
     file
-    which
-    tree
     gnused
     gnutar
     gawk
     zstd
     gnupg
-
     # nix related
     #
     # it provides the command `nom` works just like `nix`
     # with more details log output
     nix-output-monitor
-
     # productivity
     hugo # static site generator
     glow # markdown previewer in terminal
-
+    # tops
     btop  # replacement of htop/nmon
     iotop # io monitoring
     iftop # network monitoring
-
     # system call monitoring
     strace # system call monitoring
     ltrace # library call monitoring
-    lsof # list open files
-
     # system tools
     sysstat
     lm_sensors # for `sensors` command
@@ -103,17 +108,6 @@
     pciutils # lspci
     usbutils # lsusb
 
-    # my shieeeeet
-    htop
-    kubectl
-    lazygit
-    # vimPlugins.LazyVim
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })  
-    nodejs
-    stow
-    tmux
-    tokei
-    yarn
 
   ];
 
