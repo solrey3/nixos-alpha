cc7911e73616a89948b38497abde54d7c75b695f
Solito Reyes III <solrey3@solrey3.com>
2024-08-14 21:36:04 -0400

    Add pasystray

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,6 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
@@ -92,6 +91,7 @@
     packages = with pkgs; [
     #  thunderbird
       pavucontrol
+      pasystray
     ];
   };
 
